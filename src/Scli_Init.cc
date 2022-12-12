1e1f2c67f5a30b20f50a114937452aac1824bdb9
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-12-12 15:26:29 +0000

    use xmake for building && some new codes in src/Scli_Init.cc

--- src/Scli_Init.cc
+++ src/Scli_Init.cc
@@ -1,10 +1,14 @@
 #include "termcolor/termcolor.hpp"
 #include "Scli_Init.hh"
+
 #include <iostream>
 #include <string>
+#include <regex>
+#include <filesystem>
 
-std::string doPrompt(std::string text, std::string defValue = "") {
-	std::cout << termcolor::bold << text + ' '
+std::string prompt(std::string text, std::string defValue = "") {
+	std::cout // V //
+		<< termcolor::bold << text + ' '
 		<< termcolor::grey << '(' + defValue + ')'
 		<< termcolor::reset
 		<< termcolor::bold << ": "
@@ -22,10 +26,29 @@ std::string latestLuaVersion = "5.4.4";
 std::string latestLuarocksVersion = "3.9.2";
 
 cli_Init::SlyInfo_t cli_Init::promptInit() {
-	std::string luaVersion = doPrompt("lua version",latestLuaVersion);
-	std::string luarocksVersion = doPrompt("luarocks version",latestLuarocksVersion);
-	std::cout << "lua: " + luaVersion
-		<< "\nluarocks: " + luarocksVersion << std::endl;
+	std::string luaVersion = prompt("lua version",latestLuaVersion);
+	std::string luarocksVersion = prompt("luarocks version",latestLuarocksVersion);
+start_luaFile:
+	std::string luaFile = prompt("lua file");
+	
+	if (! std::filesystem::exists(luaFile)) {
+		std::cerr << termcolor::red << termcolor::bold
+			<< "invalid path to lua file!\n"
+			<< termcolor::reset;
+		goto start_luaFile;
+	}
 
+	std::cmatch _;
+	if (! std::regex_match(luaFile.c_str(),_,(std::regex) ".+?\\.lua$")) {
+		std::cerr << termcolor::red << termcolor::bold
+			<< "not a lua file!\n"
+			<< termcolor::reset;
+		goto start_luaFile;
+	}
+
+	std::cout 
+		<< "lua: " + luaVersion + '\n'
+		<< "luarocks: " + luarocksVersion + '\n'
+		<< "lua file: " + luaFile << std::endl;
 	return (cli_Init::SlyInfo_t) {};
 }
