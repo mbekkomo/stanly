f97477bc6bab91b3ec7433b572624557bd7ed42d
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-12-23 09:19:26 +0800

    idk (see diff)

--- src/stanly.cc
+++ src/stanly.cc
@@ -2,12 +2,10 @@
 #include <ostream>
 #include <string>
 #include <vector>
-#include "Scompile.hh"
 
 int main() {
 	std::cout << "Hello Stanly!" << std::endl;
 	std::cout << "Testing:\n" << std::endl;
 
-	compile::genC("");
 	return 0;
 }
